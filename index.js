b8d107cdb7123222ff2a2729573606abb3b1a2e5
Harsh Karn <harshkarn29@gmail.com>
2021-06-26 20:04:47 +0530

    features/ added broadcasting event

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ io.on("connection", (socket) => {
     console.log("user disconnected");
   });
   socket.on("chat message", (msg) => {
-    console.log("message: ", msg);
+    io.sockets.emit("chat message", msg);
+    //socket.broadcast.emit(msg);
   });
 });
 
