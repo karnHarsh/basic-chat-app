53ab8205b0324a6778b6a903347df744db6683f4
Harsh Karn <harshkarn29@gmail.com>
2021-06-26 18:52:09 +0530

    features/ added socket.io and initialised in backend

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get("/", (req, res) => {
   //res.send("<h1>Hello World</h1>");
   res.sendFile(__dirname + "/index.html");
+});
+
+//.on is for listening to events
+io.on("connection", (socket) => {
+  console.log("a user added");
 });
 
 server.listen(3000, () => {
